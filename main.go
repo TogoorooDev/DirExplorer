5b5f2262e44afd7a4ed9b04d02e97769fe98c6d3
TogoorooDev <hens25252@protonmail.com>
2021-07-28 20:35:45 -0700

    added sessions and image thumbnails

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"io"
 	"os"
 	"fmt"
+	"time"
 	"io/ioutil"
 	"strings"
 	"net/http"
 	"html/template"
+
+	"github.com/golangcollege/sessions"
 )
 
 type fileinfo_internal struct{
@@ -17,198 +20,205 @@ type fileinfo_internal struct{
 	
 }
 
-func main(){
-	// Config
+// Config
+var directory = "example/"
+var port = ":8080"
 
-	// The directory to serve
-	const directory = "example/"
+// Session
+var session *sessions.Session
+var secret = []byte("u46IpCV9y5Vlur8YvODJEhgOY8m9JVE4")
 
-	// The port to run server on. You may want to make this different from 80
-	const port = ":8080"
+func render(w http.ResponseWriter, r *http.Request){
+	dir := strings.Replace(r.URL.Path, "/", "", 1)
+	pubdir := strings.TrimPrefix(dir, directory)
 
-	handler := http.NewServeMux()
+	if dir == "" { dir = "/" }
+	dir = directory + dir
 
-	handler.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
-		dir := strings.Replace(r.URL.Path, "/", "", 1)
-		pubdir := strings.TrimPrefix(dir, directory)
+	fileinfo, err := os.Stat(dir)
 
-		if dir == "" { dir = "/" }
-		dir = directory + dir
+	if err != nil {
+		//404
+		fmt.Println(err.Error())
+		fmt.Println("Rendering 404 now...")
+		w.WriteHeader(http.StatusNotFound)
 
-		fileinfo, err := os.Stat(dir)
+		filestruct := struct {
+			Filename string
+		} {
+			Filename: pubdir}
+		
+		tmpl, _ := template.ParseFiles("templates/404.html")
+		tmpl.Execute(w, filestruct)
+		
+		return
+	}
 
-		if err != nil {
-			//404
-			fmt.Println(err.Error())
-			fmt.Println("Rendering 404 now...")
-			w.WriteHeader(http.StatusNotFound)
+	if fileinfo.IsDir(){
+		//io.WriteString(w, fmt.Sprintf("%s is a directory", dir))
 
-			filestruct := struct {
-				Filename string
-			} {
-				Filename: pubdir}
-			
-			tmpl, _ := template.ParseFiles("templates/404.html")
-			tmpl.Execute(w, filestruct)
-			
+		/*var pubdir_ends byte
+
+		if pubdir == "/"{
+			pubdir_ends = '/'
+		}else {
+			pubdir_ends = pubdir[len(pubdir) - 1]
+		}
+
+		fmt.Printf("Dir is %s\nDir ends with: %s\n", pubdir, string(pubdir_ends))*/
+		/*
+		if string(pubdir_ends) != "/" {
+			redir := string(pubdir + "/")
+			fmt.Printf("Redirecting to %s", redir)
+			http.Redirect(w, r, redir, http.StatusPermanentRedirect)
 			return
 		}
-
-		if fileinfo.IsDir(){
-			//io.WriteString(w, fmt.Sprintf("%s is a directory", dir))
-
-			/*var pubdir_ends byte
-
-			if pubdir == "/"{
-				pubdir_ends = '/'
-			}else {
-				pubdir_ends = pubdir[len(pubdir) - 1]
-			}
-
-			fmt.Printf("Dir is %s\nDir ends with: %s\n", pubdir, string(pubdir_ends))*/
-			/*
-			if string(pubdir_ends) != "/" {
-				redir := string(pubdir + "/")
-				fmt.Printf("Redirecting to %s", redir)
-				http.Redirect(w, r, redir, http.StatusPermanentRedirect)
-				return
-			}
-			*/
-		
-			files, err := ioutil.ReadDir(dir)
-			if err != nil {
-				fmt.Println(err)
-				w.Header().Set("Content-Type", "text/plain")
-				io.WriteString(w, err.Error())
-			}
-
-			//var files_arr []string
-
-			/*for _, file := range files {
-				var filename string
-				if file.IsDir() {
-					filename = file.Name() + "/"
-				} else {
-					filename = file.Name()
-				}
-				files_arr = append(files_arr, filename)
-			}*/
-
-			var fileinfo_arr []fileinfo_internal
-			
-			for _, fileinfo := range files {
-				var ext string
-				if strings.Contains(fileinfo.Name(), ".") {
-					exts := strings.Split(fileinfo.Name(), ".")
-					ext = exts[len(exts) - 1]
-				}else {
-					ext = ""
-				}
-			
-				fileinfo_to_append := fileinfo_internal {
-					Name: fileinfo.Name(),
-					Ext: ext,
-					Dir: fileinfo.IsDir()}
-				fileinfo_arr = append(fileinfo_arr, fileinfo_to_append)
-				fmt.Printf("extension: %s\n", ext)
-			}
-
-			topmost_dir_arr := strings.Split(pubdir, "/")
-			topmost_dir := topmost_dir_arr[len(topmost_dir_arr) - 1]
-			dot_dot := strings.TrimSuffix(pubdir, topmost_dir + "/")
-
-			if r.Method == "POST" {
-				r.ParseForm()
-				thumbnail_string := r.FormValue("thumbnails")
-
-				//fmt.Printf("thumbnail_bool: %s\n", thumbnail_bool)
-				var thumbnail_bool bool
-				if thumbnail_string == "false" {
-					thumbnail_bool = false
-				}else {
-					thumbnail_bool = true
-				}
-				
-				dirstruct := struct {
-					Dirname string
-					Filenames []fileinfo_internal
-					Dotdot string
-					Thumbnails bool
-				}{	
-					Dirname: pubdir,
-					Filenames: fileinfo_arr,
-					Dotdot: dot_dot,
-					Thumbnails: thumbnail_bool}
-
-				tmpl, err := template.ParseFiles("templates/dir.html")
-
-				if err != nil {
-					fmt.Println(err)
-					w.WriteHeader(http.StatusInternalServerError)
-					io.WriteString(w, "Render Error")
-					return
-
-				}
-
-				tmpl.Execute(w, dirstruct)		
-			}else { // Request is GET
-				dirstruct := struct {
-					Dirname string
-					Filenames []fileinfo_internal
-					Dotdot string
-					Thumbnails bool
-				}{	
-					Dirname: pubdir,
-					Filenames: fileinfo_arr,
-					Dotdot: dot_dot,
-					Thumbnails: true}
-				
-
-				tmpl, err := template.ParseFiles("templates/dir.html")
-
-				if err != nil {
-					fmt.Println(err)
-					w.WriteHeader(http.StatusInternalServerError)
-					io.WriteString(w, "Render Error")
-					return
-
-				}
-
-				tmpl.Execute(w, dirstruct)		
-			}
-			
-			//w.Header().Set("Content-Type", "text/plain")
-			//io.WriteString(w, out)
-
-		}else{ // Requested content is a file
-			/*io.WriteString(w, fmt.Sprintf("%s is a file", dir))*/
-			file, err := os.Open(dir)
-			
-			if err != nil {	
+		*/
+	
+		files, err := ioutil.ReadDir(dir)
+		if err != nil {
 			fmt.Println(err)
-				w.Header().Set("Content-Type", "text/plain")
-				io.WriteString(w, fmt.Sprintf("Error opening file"))
+			w.Header().Set("Content-Type", "text/plain")
+			io.WriteString(w, err.Error())
+		}
+		
+		var fileinfo_arr []fileinfo_internal
+		for _, fileinfo := range files {
+			var ext string
+			if strings.Contains(fileinfo.Name(), ".") {
+				exts := strings.Split(fileinfo.Name(), ".")
+				ext = exts[len(exts) - 1]
+			}else {
+				ext = ""
 			}
-
-			file_buffer, err := ioutil.ReadAll(file)
-			if err != nil {
-				w.Header().Set("Content-Type", "text/plain")
-				io.WriteString(w, fmt.Sprintf("Error opening file"))
-			}
-
-			content_type := http.DetectContentType(file_buffer)
-
-			w.Header().Set("Content-Type", content_type)
-			http.ServeFile(w, r, dir)
-			
+		
+			fileinfo_to_append := fileinfo_internal {
+				Name: fileinfo.Name(),
+				Ext: ext,
+				Dir: fileinfo.IsDir()}
+			fileinfo_arr = append(fileinfo_arr, fileinfo_to_append)
 		}
 
+		topmost_dir_arr := strings.Split(pubdir, "/")
+		topmost_dir := topmost_dir_arr[len(topmost_dir_arr) - 1]
+		dot_dot := strings.TrimSuffix(pubdir, topmost_dir + "/")
+
+		if r.Method == "POST" {
+			r.ParseForm()
+			thumbnail_string := r.FormValue("thumbnails")
+
+			session.Put(r, "thumbnail", thumbnail_string)
+			
+			//fmt.Printf("thumbnail_bool: %s\n", thumbnail_bool)
+			var thumbnail_bool bool
+			if thumbnail_string == "false" {
+				thumbnail_bool = false
+			}else {
+				thumbnail_bool = true
+			}
+			
+			dirstruct := struct {
+				Dirname string
+				Filenames []fileinfo_internal
+				Dotdot string
+				Thumbnails bool
+			}{	
+				Dirname: pubdir,
+				Filenames: fileinfo_arr,
+				Dotdot: dot_dot,
+				Thumbnails: thumbnail_bool}
+
+			tmpl, err := template.ParseFiles("templates/dir.html")
+
+			if err != nil {
+				fmt.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				io.WriteString(w, "Render Error")
+				return
+
+			}
+
+			tmpl.Execute(w, dirstruct)		
+		}else { // Request is GET
+
+			thumbnail_string := session.GetString(r, "thumbnail")
+
+			if thumbnail_string == "" {
+				thumbnail_string = "true"
+			}
+
+			var thumbnail_bool bool
+			
+			if thumbnail_string == "true" {
+				thumbnail_bool = true
+			}else {
+				thumbnail_bool = false
+			}
+		
+			dirstruct := struct {
+				Dirname string
+				Filenames []fileinfo_internal
+				Dotdot string
+				Thumbnails bool
+			}{	
+				Dirname: pubdir,
+				Filenames: fileinfo_arr,
+				Dotdot: dot_dot,
+				Thumbnails: thumbnail_bool}
+			
+
+			tmpl, err := template.ParseFiles("templates/dir.html")
+
+			if err != nil {
+				fmt.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				io.WriteString(w, "Render Error")
+				return
+
+			}
+
+			tmpl.Execute(w, dirstruct)		
+		}
+		
 		//w.Header().Set("Content-Type", "text/plain")
-		//io.WriteString(w, fmt.Sprintf("Welcome to %s\n", dir))
+		//io.WriteString(w, out)
 
-	})
+	}else{ // Requested content is a file
+		/*io.WriteString(w, fmt.Sprintf("%s is a file", dir))*/
+		file, err := os.Open(dir)
+		
+		if err != nil {	
+		fmt.Println(err)
+			w.Header().Set("Content-Type", "text/plain")
+			io.WriteString(w, fmt.Sprintf("Error opening file"))
+		}
 
-	err := http.ListenAndServe(port, handler)
+		file_buffer, err := ioutil.ReadAll(file)
+		if err != nil {
+			w.Header().Set("Content-Type", "text/plain")
+			io.WriteString(w, fmt.Sprintf("Error opening file"))
+		}
+
+		content_type := http.DetectContentType(file_buffer)
+
+		w.Header().Set("Content-Type", content_type)
+		http.ServeFile(w, r, dir)
+		
+	}
+
+	//w.Header().Set("Content-Type", "text/plain")
+	//io.WriteString(w, fmt.Sprintf("Welcome to %s\n", dir))
+}
+
+func main(){
+	session = sessions.New(secret)
+	session.Lifetime = 3 * time.Hour
+	
+	handler := http.NewServeMux()
+
+	handler.HandleFunc("/", render)
+
+	err := http.ListenAndServe(port, session.Enable(handler))
 
 	if err != nil {
 		fmt.Println(err.Error())
